733f85a83743601527f6872e3eb81eceb1cf8457
brutal-45 <creatorsports81@gmail.com>
2026-04-30 18:57:25 +0530

    Update generate_brutalguide_pdf.py

--- generate_brutalguide_pdf.py
+++ generate_brutalguide_pdf.py
@@ -8,7 +8,7 @@ from reportlab.lib.pagesizes import A4
 from reportlab.lib.units import inch, cm
 from reportlab.lib import colors
 from reportlab.lib.styles import getSampleStyleSheet, ParagraphStyle
-from reportlab.lib.enums import TA_CENTER, TA_LEFT, TA_JUSTIFY
+from reportlab.lib.enums import TA_CENTER, TA_LEFT, TA_JUSTIFY 
 from reportlab.platypus import (
     SimpleDocTemplate, Paragraph, Spacer, PageBreak, Table, TableStyle,
     Image, ListFlowable, ListItem
